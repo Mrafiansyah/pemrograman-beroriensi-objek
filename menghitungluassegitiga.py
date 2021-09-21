ae2f4984a89ef14ed6d3dd6e66a84243dca46f3a
Mrafiansyah <90373940+Mrafiansyah@users.noreply.github.com>
2021-09-21 18:54:30 +0700

    Add files via upload

---
+++ menghitungluassegitiga.py
@@ -0,0 +1,7 @@
+print ("MENGHITUNG LUAS SEGITIGA")
+a = float (input("Masukkan panjang alas = "))
+t = float (input("Masukkan panjang tinggi = "))
+
+luassegitiga = 0.5*a*t
+
+print ("Luas segitiga adalah ", luassegitiga)
---
+++ menghitungvolumebalok.py
@@ -0,0 +1,8 @@
+print ("MENGHITUNG VOLUME BALOK")
+p = float (input("Masukkan panjang sisi = "))
+l = float (input("Masukkan lebar sisi = "))
+t = float (input("masukkan tinggi sisi = "))
+
+volumebalok = p*l*t
+
+print ("volume balok adalah ", volumebalok)
